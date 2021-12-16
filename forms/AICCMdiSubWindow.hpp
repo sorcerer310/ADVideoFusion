bca45403ee12ed607e3369368c42de46e03baad2
fc <fengchong@126.com>
2021-12-16 20:53:22 +0800

    增加图标和子窗口文件

---
+++ forms/AICCMdiSubWindow.hpp
@@ -0,0 +1,16 @@
+﻿#ifndef AICCMDISUBWINDOW_H
+#define AICCMDISUBWINDOW_H
+
+#include <QMdiSubWindow>
+
+class AICCMdiSubWindow : public QMdiSubWindow
+{
+    Q_OBJECT
+public:
+    AICCMdiSubWindow(){}
+    ~AICCMdiSubWindow(){}
+
+
+};
+
+#endif // AICCMDISUBWINDOW_H
